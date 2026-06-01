4bc4e4e9fcdb87dbcda9c07f9e7afb6c3ca761a2
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-06-01 13:06:47 +0545

    Create CountDigitFunction.java

---
+++ Functions/CountDigitFunction.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CountDigitsFunction {
+    public static int countDigit(int n){
+        int count = 0;
+         while(n > 0){
+            count++;
+            n = n / 10;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int n = sc.nextInt();
+        System.out.println(countDigit(n));
+    }
+}
