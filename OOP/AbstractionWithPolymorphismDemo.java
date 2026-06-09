f11ecc6bbbd9b9b01058a29676f168fd237847d1
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-06-09 11:25:15 +0545

    Add abstraction and polymorphism example in Java

---
+++ OOP/AbstractionWithPolymorphismDemo.java
@@ -0,0 +1,16 @@
+//Abstraction + Run-time polymorphism
+abstract class Animal{
+    abstract void sound();
+}
+class Dog extends Animal{
+    @Override
+    void sound(){
+        System.out.println("Dog is Barking...");
+    }
+}
+public class AbstractionWithPolymorphismDemo {
+    public static void main(String[] args) {
+        Animal a = new Dog(); // polymorphism
+        a.sound();
+    }
+}
