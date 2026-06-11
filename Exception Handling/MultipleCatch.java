d816d0d185c6c7354ea0adcbfd8a76e1f805e41f
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-06-11 15:20:02 +0545

    Implement multiple catch blocks for exceptions

---
+++ Exception Handling/MultipleCatch.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+
+public class MultipleCatch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print("Enter size: ");
+                int n = sc.nextInt();
+                int[] arr = new int[n];
+                System.out.print("Enter array elements: ");
+                for (int i = 0; i < arr.length; i++) {
+                    arr[i] = sc.nextInt();
+                }
+                System.out.println(arr[0] / arr[1]);
+
+                System.out.println(arr[5]);
+            } catch (ArithmeticException e) {
+                System.out.println(e);
+                System.out.println(e.getMessage());
+                System.out.println("Error!");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
+                System.out.println(e.getMessage());
+                System.out.println("Error!");
+            } catch (InputMismatchException e) {
+                System.out.println(e);
+                System.out.println(e.getMessage());
+                System.out.println("Error!");
+            }
+            System.out.println();
+            System.out.println("Program end!");
+        }
+    }
+}
