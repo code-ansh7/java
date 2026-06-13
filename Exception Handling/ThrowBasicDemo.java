5432ba1b4b6cd7b69cad473997f1b0ca723973f9
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-06-13 12:29:51 +0545

    Add basic example of throwing an exception

---
+++ Exception Handling/ThrowBasicDemo.java
@@ -0,0 +1,15 @@
+// Basic throw example
+
+public class ThrowBasicDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Program Started");
+
+        // Manually generating exception
+        throw new ArithmeticException("Error Generated");
+
+        // Ye line kabhi execute nahi hogi
+        // System.out.println("Program End");
+    }
+}
